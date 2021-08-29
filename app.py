79183f838dff108a365ae947c69386bc1b5a7b70
NXQ7 <anthonynhem@gmail.com>
2021-08-29 05:29:57 -0700

    first solo update

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/register/')
+def register():
+    return render_template("register.html")
+
 if __name__=="__main__":
     app.run(debug=True)
