09f98395cf99e16ed7783a484c0042b0c2430236
NXQ7 <anthonynhem@gmail.com>
2021-08-29 08:39:29 -0700

    login and register change

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def about():
 def register():
     return render_template("register.html")
 
+@app.route('/login/')
+def login():
+    return render_template("login.html")    
+
 if __name__=="__main__":
     app.run(debug=True)
